a70eedc1023a59a0b6eb0a92a270621904fd6bcb
HaoYuLiu0725 <108288501+HaoYuLiu0725@users.noreply.github.com>
2022-11-20 00:38:51 +0800

    Update main_tel2022.cpp

--- src/main_program/src/main_tel2022.cpp
+++ src/main_program/src/main_tel2022.cpp
@@ -348,7 +348,7 @@ public:
             {
                 cout << "No Relative Script Can Be Read !" << endl;
             }
-            cout << "Script: " << whichScript << "get" << endl;
+            cout << "Script: " << whichScript << " get" << endl;
             now_Status = SETUP;
             readScript = false;
         }
